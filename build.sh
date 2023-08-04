b5910289c8cc5623822a035f5653d60d2c531bab
nyatado98 <danndong080@gmail.com>
2023-08-04 13:24:18 +0300

    build changes

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
+ #!/bin/bash
+ 
  python3.9 -m pip install -r requirements.txt
 
- echo"collecting static files"
- python3.9 manage.py collectstatic --noinput --clear
+
+ python3.9 manage.py collectstatic 
