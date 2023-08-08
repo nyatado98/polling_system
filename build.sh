495ed5fa05f9ab4be5516f5efb3425ff963b3b42
nyatado98 <danndong080@gmail.com>
2023-08-08 12:19:14 +0300

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
  python3.9 -m venv virtual
- source virtual/bin/activate
+ source virtual/Scripts/activate
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
