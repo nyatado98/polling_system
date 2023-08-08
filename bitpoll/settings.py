7887c60fa375c9692be6e8160c5ab7c514fb96f9
nyatado98 <danndong080@gmail.com>
2023-08-08 11:32:05 +0300

    staticfiles

--- bitpoll/settings.py
+++ bitpoll/settings.py
@@ -34,9 +34,9 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, '_static')
 # STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, '/bitpoll/base/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
