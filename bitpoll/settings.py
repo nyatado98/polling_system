3ca2ea7d73f52419cf27e5eccd2b386f370deddc
nyatado98 <danndong080@gmail.com>
2023-08-14 09:32:19 +0300

    files

--- bitpoll/settings.py
+++ bitpoll/settings.py
@@ -36,11 +36,11 @@ DEBUG = True
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'static'),
+os.path.join(ROOT_DIR, 'stat'),
 
 ]
 
-STATIC_ROOT = os.path.join(ROOT_DIR, 'stat')
+STATIC_ROOT = os.path.join(ROOT_DIR, '_static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
