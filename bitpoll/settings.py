409252aad9b3dd8716f725c3b08066793e143d82
nyatado98 <danndong080@gmail.com>
2023-08-14 08:56:52 +0300

    new

--- bitpoll/settings.py
+++ bitpoll/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = os.path.join(ROOT_DIR, '_media')
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = '/media/'
 
-DEBUG = False
+DEBUG = True
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
