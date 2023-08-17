15586779218b1f553c2d0440294d88526dd56f03
nyatado98 <danndong080@gmail.com>
2023-08-17 13:34:37 +0300

    vital

--- bitpoll/settings.py
+++ bitpoll/settings.py
@@ -23,12 +23,12 @@ ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-
+MEDIA_ROOT = os.path.join(ROOT_DIR, '_media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-
+MEDIA_URL = '/media/'
 
 
 # Absolute path to the directory static files should be collected to.
