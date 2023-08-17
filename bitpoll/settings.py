51a6892ff67b065891694004ab3a7dfc97b9663b
nyatado98 <danndong080@gmail.com>
2023-08-17 18:35:01 +0300

    ,,,,,,

--- bitpoll/settings.py
+++ bitpoll/settings.py
@@ -48,7 +48,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static', 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 ALLOWED_HOSTS = ['*']
 
