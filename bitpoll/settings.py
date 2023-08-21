f6c9bbf9d002c45055a169044c5e38d1f2059a7b
nyatado98 <danndong080@gmail.com>
2023-08-21 12:42:04 +0300

    ...

--- bitpoll/settings.py
+++ bitpoll/settings.py
@@ -36,7 +36,7 @@ MEDIA_URL = '/media/'
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'static/static'),
+os.path.join(ROOT_DIR, '_static'),
 
 ]
 
